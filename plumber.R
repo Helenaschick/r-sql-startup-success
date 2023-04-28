70e102e2a1351199c1cc68c954239272b091e3c7
Helenaschick <hs107@hdm-stuttgart.de>
2023-04-28 21:25:55 +0200

    Plumber continued

--- plumber.R
+++ plumber.R
@@ -1,8 +1,19 @@
 # plumber.R
 
-#* Echo the parameter that was sent in
-#* @param msg The message to echo back.
-#* @get /echo
-function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
+model <- readRDS("model.rds")
+
+#* Plot a histogram of the relationships
+#* @serializer png
+#* @get /plotwt
+function(){
+  hist(train_data$relationships)
+}
+
+#* Returns the probability whether the car has a manual transmission
+#* @param relationships relationships
+#* @param funding_total_usd funding
+#* @post /acquired
+function(relationships, funding_total_usd){
+  newdata <- data.frame(relationships = as.numeric(relationships), funding_total_usd = as.numeric(funding_total_usd))
+  predict(model, newdata, type = "response")
 }
