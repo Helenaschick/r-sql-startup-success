9ef450f4eebe1c6bcc9c379ac80b3f330e917d4b
Helenaschick <hs107@hdm-stuttgart.de>
2023-05-01 21:58:01 +0200

    Deploy and ppt continued

--- plumber.R
+++ plumber.R
@@ -9,11 +9,12 @@ function(){
   hist(train_data$relationships)
 }
 
-#* Returns the probability whether the car has a manual transmission
+#* Returns the probability whether a startup will be acquired
 #* @param relationships relationships
 #* @param funding_total_usd funding
-#* @post /acquired
+#* @post /status
 function(relationships, funding_total_usd){
-  newdata <- data.frame(relationships = as.numeric(relationships), funding_total_usd = as.numeric(funding_total_usd))
+  newdata <- data.frame(relationships = as.numeric(relationships), 
+                        funding_total_usd = as.numeric(funding_total_usd))
   predict(model, newdata, type = "response")
 }
